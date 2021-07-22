8e6d6b3a732b5126bec76b46636b276db3b33b3c
Hannah Stahl <hls62@cornell.edu>
2021-07-21 22:14:07 -0500

    Created Sanity Studio with blog post type

---
+++ schemas/blogPost.js
@@ -0,0 +1,32 @@
+export default {
+  title: "Blog Post",
+  name: "blogPost",
+  type: "document",
+  fields: [
+    {
+      title: "Title",
+      name: "title",
+      type: "string",
+      validation: Rule => Rule.required(),
+    },
+    {
+      title: "Date",
+      name: "date",
+      type: "date",
+      validation: Rule => Rule.required(),
+    },
+    {
+      title: "Image",
+      name: "image",
+      type: "image",
+      validation: Rule => Rule.required(),
+    },
+    {
+      title: "Article",
+      name: "article",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: Rule => Rule.required(),
+    },
+  ]
+};
